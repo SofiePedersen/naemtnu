2c0b9ade465e14722d207d71d3a5f8269a46c481
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2026-05-06 10:24:40 +0200

    update herosection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -26,7 +26,6 @@
 
 .HeroSection__headline__text {
     font-family: 'Comfortaa', sans-serif;
-    font-size: 3.5rem;
     color: $color-kelp-green;
     margin-bottom: 1rem;
     margin-right: 18.75rem;
@@ -35,7 +34,6 @@
 .HeroSection__paragraph__text {
     font-family: 'Montserret', sans-serif;
     color: $color-kelp-green;
-    font-size: 1.25rem;
     margin-bottom: 2rem;
     margin-right: 5rem;
     margin-top: 1.3rem;
