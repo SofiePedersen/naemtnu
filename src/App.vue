91eed278e9e4621ec84eb510f3580ae383041e47
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2026-05-06 09:46:33 +0200

    Header done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import Header from "./components/Header.vue";
 </script>
 
 <template>
- 
+    <main>
+  <Header />
+    </main>
 </template>
 
 <style scoped>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,29 @@
 </script>
 
 <template>
-
+    <header>
+        <img src="../assets/icons/header-logo.svg" alt="Logo" class="logo">
+    </header>
 </template>
 
 <style lang="scss" scoped>
 @import "../assets/main.scss";
+
+header {
+    display: flex;
+    justify-content: start;
+    background-color: $color-foam-blue;
+    padding-top: 2rem;
+}
+
+.logo {
+    margin-left: 1rem;
+    margin-right: 18.75rem;
+    margin-top: 2rem;
+    margin-bottom: 3.75rem;
+    // jeg gav rem på 4 fordi størrelsen ellers blev helt ødelagt //
+    height: 4rem;
+    width: 9.7rem;
+}
 
 </style>
