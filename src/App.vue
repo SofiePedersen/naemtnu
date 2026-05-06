c038830c8a00b42e50b0268438fdad1219bb3c0e
madsmads4 <madsolsen213@gmail.com>
2026-05-06 14:07:48 +0200

    Contact.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
 import HeroSection from "./components/HeroSection.vue";
+import Contact from "./components/Contact.vue";
 </script>
 
 <template>
   <main>
     <Header />
     <HeroSection />
+    <Contact />
     <Footer />
   </main>
 </template>
---
+++ src/components/Contact.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="contact">
+    <h4>Vil du også have det næmt? Så lad os tage en snak.</h4>
+    <h2>Din virksomhed fortjener en hjemmeside, der arbejder for dig</h2>
+    <p>Hiv fat i Sebastian og få en ærlig snak om din hjemmeside.</p>
+    <p>
+      Vi lytter, spørger ind - og finder sammen en løsning, der gør det næmt for
+      dig. Nu.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/main.scss";
+</style>
