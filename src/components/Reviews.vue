75e2e975c6f24eaf1788c592078aa59aae7d94af
madsmads4 <madsolsen213@gmail.com>
2026-05-08 14:15:53 +0200

    Update inden API bliver sat ind

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,10 +1,22 @@
-<script setup></script>
+<script setup>
+import Arrow from "@/assets/icons/arrow.svg";
+</script>
 
 <template>
-  <div class="review">
-    <h4>Det ikke kun os selv, der siger, det er næmt...</h4>
-    <h2>Det siger vores kunder også</h2>
-  </div>
+  <main>
+    <div class="review">
+      <h4>Det ikke kun os selv, der siger, det er næmt...</h4>
+      <h2>Det siger vores kunder også</h2>
+
+      <div class="review__slider">
+        <button src><img :src="Arrow" alt="Pil" /></button>
+
+        <div class="review__card"></div>
+
+        <button src><img :src="Arrow" alt="Pil" /></button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
@@ -16,5 +28,34 @@
   display: flex;
   flex-direction: column;
   gap: 1rem;
+}
+
+.review__slider {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.review__slider button {
+  width: 3.125rem;
+  height: 3.125rem;
+  border-radius: 50%;
+  border: none;
+  background-color: $color-cloudy-white;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  cursor: pointer;
+  padding: 0;
+}
+
+.review__slider button img {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+.review__slider button:last-child img {
+  transform: rotate(180deg);
 }
 </style>
