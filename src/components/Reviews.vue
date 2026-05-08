80ceead5ad0b33dfdb86147b473740fa617a2983
madsmads4 <madsolsen213@gmail.com>
2026-05-08 12:39:08 +0200

    Reviews started

---
+++ src/components/Reviews.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="review">
+    <h4>Det ikke kun os selv, der siger, det er næmt...</h4>
+    <h2>Det siger vores kunder også</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/main.scss";
+.review {
+  background-color: $color-kelp-green;
+  color: $color-cloudy-white;
+  padding: 4rem 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
