6a9906b77de1175628eb2748a55f29e4c93d7051
madsmads4 <madsolsen213@gmail.com>
2026-05-20 14:02:23 +0200

    Update HeroSection.vue

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -54,12 +54,19 @@ const fetchPageSpeedData = async () => {
     <div class="HeroSection__wrapper">
       <div class="HeroSection" v-if="!resultData && !isLoading">
         <h1 class="HeroSection__text__h1">Tjek Din Hjemmesides SEO</h1>
-        <p class="HeroSection__text__p">Find ud af, hvordan din hjemmeside rangerer i søgninger og få tips til forbedring.</p>
+        <p class="HeroSection__text__p">
+          Tjek om din hjemmeside har grundlæggende SEO i orden. Det giver dig en
+          hurtig fornemmelse af, hvor din hjemmeside står lige nu.
+        </p>
       </div>
       <div class="HeroSection" v-if="resultData">
         <h2 class="HeroSection__text__h1">Hvad betyder din score?</h2>
-        <p class="HeroSection__text__p">Dit fundament er lagt, men der er altid plads til optimering. Ved at finjustere dit indhold og styrke din tekniske SEO, kan vi sikre, at 
-            din forretning bliver fundet af de helt rigtige kunder, når de søger efter dine ydelser.</p>
+        <p class="HeroSection__text__p">
+          Dit fundament er lagt, men der er altid plads til optimering. Ved at
+          finjustere dit indhold og styrke din tekniske SEO, kan vi sikre, at
+          din forretning bliver fundet af de helt rigtige kunder, når de søger
+          efter dine ydelser.
+        </p>
         <div class="wrapper">
           <div class="outer">
             <div class="inner">
@@ -67,7 +74,12 @@ const fetchPageSpeedData = async () => {
             </div>
           </div>
           <svg width="20rem" height="20rem" viewBox="0 0 320 320">
-            <circle cx="160" cy="160" r="145" :style="{ strokeDashoffset: currentOffset }"/>
+            <circle
+              cx="160"
+              cy="160"
+              r="145"
+              :style="{ strokeDashoffset: currentOffset }"
+            />
           </svg>
         </div>
         <h2 class="HeroSection__text__h2">SEO Score</h2>
@@ -76,12 +88,45 @@ const fetchPageSpeedData = async () => {
         <span class="loader"></span>
       </div>
       <div class="HeroSection__button--SEO HeroSection__button--SEOpreloaded">
-        <p v-if="!resultData && !isLoading" class="HeroSection__text__pbold">Link til hjemmeside:</p>
-        <input v-if="!resultData && !isLoading" type="text" v-model="targetUrl" @keydown.enter="fetchPageSpeedData" placeholder="Indsæt dit fulde link her..."/>
-        <button v-if="!resultData" class="btn__green btn__SEO" type="submit" :disabled="isLoading" id="submit-btn" @click="fetchPageSpeedData">{{ isLoading ? "Indlæser..." : "Tjek min SEO" }}</button>
+        <p v-if="!resultData && !isLoading" class="HeroSection__text__pbold">
+          Link til hjemmeside:
+        </p>
+        <input
+          v-if="!resultData && !isLoading"
+          type="text"
+          v-model="targetUrl"
+          @keydown.enter="fetchPageSpeedData"
+          placeholder="Indsæt dit fulde link her..."
+        />
+        <button
+          v-if="!resultData"
+          class="btn__green btn__SEO"
+          type="submit"
+          :disabled="isLoading"
+          id="submit-btn"
+          @click="fetchPageSpeedData"
+        >
+          {{ isLoading ? "Indlæser..." : "Tjek min SEO" }}
+        </button>
         <div class="result-btns-data">
-          <a class="btn__green" v-if="resultData" type="submit":disabled="isLoading" id="submit-btn" href="https://www.linkedin.com/company/naemt-nu/posts/?feedView=all">Kontakt os</a>
-          <button v-if="resultData" class="btn__white" type="submit" id="submit-btn" @click="reloadPage">Søg igen</button>
+          <a
+            class="btn__green"
+            v-if="resultData"
+            type="submit"
+            :disabled="isLoading"
+            id="submit-btn"
+            href="https://www.linkedin.com/company/naemt-nu/posts/?feedView=all"
+            >Kontakt os</a
+          >
+          <button
+            v-if="resultData"
+            class="btn__white"
+            type="submit"
+            id="submit-btn"
+            @click="reloadPage"
+          >
+            Søg igen
+          </button>
         </div>
       </div>
     </div>
@@ -91,13 +136,18 @@ const fetchPageSpeedData = async () => {
 <style lang="scss" scoped>
 @import "../assets/main.scss";
 
+.HeroSection__wrapper {
+  background-color: $color-foam-blue;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+
 .HeroSection {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  padding-left: 1rem;
-  padding-right: 1rem;
+
   background-color: $color-foam-blue;
 }
 
@@ -130,9 +180,6 @@ const fetchPageSpeedData = async () => {
 }
 
 .HeroSection__button--SEO {
-  padding-left: 1rem;
-  padding-right: 1rem;
-  // padding-top: 3rem;
   padding-bottom: 3rem;
   width: 100%;
   display: flex;
@@ -282,12 +329,13 @@ circle {
     padding: 0rem 1rem;
     gap: 0rem;
   }
-  
+
   .result-btns-data {
     gap: 0.5rem;
     margin-bottom: 3rem;
 
-    .btn__green, .btn__white {
+    .btn__green,
+    .btn__white {
       padding: 1rem 1.5rem;
       width: 50%;
       text-align: center;
@@ -304,12 +352,13 @@ circle {
     padding: 0rem 1rem;
     gap: 0rem;
   }
-  
+
   .result-btns-data {
     gap: 0.5rem;
     margin-bottom: 3rem;
 
-    .btn__green, .btn__white {
+    .btn__green,
+    .btn__white {
       padding: 1rem 1.5rem;
       width: 50%;
       font-size: 0.875rem;
@@ -356,56 +405,27 @@ circle {
     align-items: center;
     justify-content: center;
   }
-
 }
 
 @media (min-width: 1200px) {
-  .HeroSection {
-    padding: 0;
-  }
-
-  .HeroSection__image {
-    display: flex;
-    position: absolute;
-    justify-content: end;
-  }
-
-  .HeroSection__text__p {
-    display: flex;
-    flex-direction: column;
-    margin-left: 9.375rem;
-  }
-
-  .HeroSection__button--SEO {
-    justify-content: start;
+  .HeroSection__wrapper {
     padding-left: 9.375rem;
-    flex-direction: row;
-    align-items: start;
-    padding-bottom: 10rem;
-  }
-
-  .HeroSection__text__pbold {
-    padding-left: 0;
-  }
-
-  .HeroSection__text__h1 {
-    margin-left: 9.375rem;
-  }
-
-  .HeroSection__text__h2 {
-    margin-right: 9.375rem;
-    text-align: end;
+    padding-right: 9.375rem;
   }
 
   .loader {
-    margin-left: 11rem;
+    margin: 2rem;
   }
 
-  .wrapper {
-    text-align: end;
-    display: flex;
-    padding-left: 50rem;
-    margin-right: 9.375rem;
+  .HeroSection {
+    padding-bottom: 1rem;
+  }
+
+  .HeroSection__text__p {
+    align-self: flex-start;
+    text-align: left;
+    font-size: 1.5rem;
+    width: 65%;
   }
 }
 </style>
